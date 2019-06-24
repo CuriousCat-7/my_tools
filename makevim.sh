57b11ac83e8f4e1b7278013184089463cb25ea95
limingyao <limingyao@ainirobot.com>
2019-06-24 11:34:45 +0800

    rm neo vim

--- makevim.sh
+++ makevim.sh
@@ -1,15 +1,15 @@
 there=`pwd`
 # neovim
-mkdir -p ~/work
-cd ~/work
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-#./squashfs-root/usr/bin/nvim
-echo "alias vim=$HOME/work/squashfs-root/usr/bin/nvim" >> ~/.bashrc
-cd $there
-mkdir -p ~/.config
-cp -r nvim ~/.config/
+#mkdir -p ~/work
+#cd ~/work
+#curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+#chmod u+x nvim.appimage
+#./nvim.appimage --appimage-extract
+##./squashfs-root/usr/bin/nvim
+#echo "alias vim=$HOME/work/squashfs-root/usr/bin/nvim" >> ~/.bashrc
+#cd $there
+#mkdir -p ~/.config
+#cp -r nvim ~/.config/
 # vimrc
 cd ~
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
